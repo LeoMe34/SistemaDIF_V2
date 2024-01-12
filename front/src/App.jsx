adcdfdb4f31ade4a433daacd96a8c8168c71facd
Mayreli Romero <may70981@gmail.com>
2024-01-12 16:54:42 -0600

    Navbar simple

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { Footer } from './Partials/Footer'
+import { NavBarSimple } from './Partials/NavBarSimple';
+
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el archivo CSS de Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa el archivo JavaScript de Bootstrap (con Popper.js)
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -8,11 +10,10 @@ import { Toaster } from 'react-hot-toast'
 function App() {
   return (
     <div>
-      <div>
-        
-      </div>
+      <NavBarSimple/>
+
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   )
---
+++ front/src/Partials/NavBarSimple.jsx
@@ -0,0 +1,15 @@
+
+export function NavBarSimple() {
+    return (
+        <nav className="navbar div-header">
+            <div className="container-fluid">
+                <div className="encabezado">
+                    <img src="../Logos/LOGO DIF.jpeg" alt="Logo del DIF" className="img-logos mx-2" />
+                    <img src="../Logos/logo-coatza.jpg" alt="Logo del DIF-Coatzacoalcos" className="img-logos mx-2" />
+                    <img src="../Logos/LOGO-AYUNTAMIENTO-22-25.webp" alt="Logo del Ayuntamiento de Coatzacoalcos" className="img-logos mx-2" />
+                    DIF MUNICIPAL
+                </div>
+            </div>
+        </nav>
+    );
+}
