787e212d34480a70f99ddbab794930f4c3c526e1
Mayreli Romero <may70981@gmail.com>
2024-02-05 22:21:51 -0600

    Crear Empleado, front

--- front/src/Routes/Rutas.jsx
+++ front/src/Routes/Rutas.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { V_FichaTecnicaEnfermeria } from '../Views/FormatoEnfermeria/V_FichaTecnicaEnfermeria'
+import { V_CrearEmpleado } from '../Views/Empleado/V_CrearEmpleado'
 
 export function Rutas() {
     return (
@@ -8,6 +9,7 @@ export function Rutas() {
                 <div>
                     <Routes>
                         <Route path='/ficha_tecnica_enfermeria' element={<V_FichaTecnicaEnfermeria />} />
+                        <Route path='/crear_empleado' element={<V_CrearEmpleado />} />
                     </Routes>
                 </div>
             </BrowserRouter>
