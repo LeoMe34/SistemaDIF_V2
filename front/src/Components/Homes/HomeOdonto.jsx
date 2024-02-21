80d17f48b023928bcbc4f54cd1a8478160b37513
Mayreli Romero <may70981@gmail.com>
2024-02-20 22:55:46 -0600

    Miga de pan en home

--- front/src/Components/Homes/HomeOdonto.jsx
+++ front/src/Components/Homes/HomeOdonto.jsx
@@ -2,7 +2,14 @@
 export function HomeOdonto() {
     return (
         <div>
-
+            <div className="mt-3 ml-10 container">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <i class="bi bi-house-fill"></i>
+                        <li class="breadcrumb-item active" aria-current="page">Home</li>
+                    </ol>
+                </nav>
+            </div>
         </div>
     )
 }
