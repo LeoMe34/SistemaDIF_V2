cd0c2f6e6334f48df72a49cf6f31ec2e8c3ecfc4
Mayreli Romero <may70981@gmail.com>
2024-02-20 19:13:51 -0600

    Cambio de cardPaciente por CardEnfermeria

--- front/src/Components/FormatoOdonto/NotaEvolucion.jsx
+++ front/src/Components/FormatoOdonto/NotaEvolucion.jsx
@@ -1,33 +1,35 @@
-import { CardPaciente } from "../Paciente/CardPaciente"
+import { CardFichaEnfermeria } from "../FormatoEnfermeria/CardFichaEnfermeria"
 
 export function NotaEvolucion() {
     return (
         <div>
-            <div className='ml-10 container'>
-                <CardPaciente />
-            </div>
             <h2 className='subtitulo'>Nota Evolución</h2>
-            <div className="ml-10 col">
-                <textarea id="habitos" placeholder="Notas" className="text-amplio" rows="10" cols="30" />
+
+            <div className='ml-10 mt-3 mb-3 container'>
+                <CardFichaEnfermeria />
             </div>
-            <div className="ml-10 col">
-                <label className="etiqueta" htmlFor="diagnostico">Diagnostico</label>
-                <textarea id="diagnostico" type="text" placeholder="diagnostico" className="entrada" rows="10" cols="30" />
-            </div>
-            <div className="ml-10 col">
-                <label className="etiqueta" htmlFor="tratamiento">Tratamiento</label>
-                <textarea id="tratamiento" type="text" placeholder="tratamiento" className="entrada" rows="10" cols="30" />
+            <div className="container">
+                <div className="col">
+                    <textarea id="habitos" placeholder="Notas" className="text-amplio" rows="5" />
+                </div>
+                <div className="mt-2 col">
+                    <label className="etiqueta" htmlFor="diagnostico">Diagnostico</label>
+                    <textarea id="diagnostico" type="text" placeholder="diagnostico" className="entrada" rows="5" />
+                </div>
+                <div className="mt-2 col">
+                    <label className="etiqueta" htmlFor="tratamiento">Tratamiento</label>
+                    <textarea id="tratamiento" type="text" placeholder="tratamiento" className="entrada" rows="5" />
+                </div>
             </div>
 
-            <div className='ml-10 mb-2 container'>
-                <div className='row'>
-                    <div className='col'>
-                        <label className='etiqueta' htmlFor="medico">Médico:</label>
-                        <input className="datos_lectura" id='medico' name='medico' type="text" readOnly />
-                        <label className='etiqueta' htmlFor="cedula">Cédula:</label>
-                        <input className="datos_lectura" id='cedula' name='cedula' type="text" readOnly />
-                        <label className='etiqueta-firma' htmlFor="firma">Firma:</label>
-                    </div>
+
+            <div className='mt-2 mb-2 container'>
+                <div className='col'>
+                    <label className='etiqueta' htmlFor="medico">Médico:</label>
+                    <input className="datos_lectura" id='medico' name='medico' type="text" readOnly />
+                    <label className='etiqueta' htmlFor="cedula">Cédula:</label>
+                    <input className="datos_lectura" id='cedula' name='cedula' type="text" readOnly />
+                    <label className='etiqueta-firma' htmlFor="firma">Firma:</label>
                 </div>
             </div>
 
