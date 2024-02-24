ee2003c6846fb21bbdad5375f0285e762e26027a
Mayreli Romero <may70981@gmail.com>
2024-02-24 14:32:19 -0600

    Actualizacion vite y modales

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ npm install bootstrap-icons
 npm install styled-components
 npm install react-icons
 npm install react-collapsible --save
+npm install sweetalert2
+npm install --save sweetalert2 sweetalert2-react-content
+#Actualizar Vite
+npm install -g create-vite 
+npm install vite-plugin-react@latest
--- front/src/Components/Empleado/CrearEmpleado.jsx
+++ front/src/Components/Empleado/CrearEmpleado.jsx
@@ -1,6 +1,6 @@
 {/*import { NavBarSimple } from "../../Partials/NavBarSimple"*/ }
 
-export function CrearEmpleado() {
+export function CrearEmpleado() {    
     return (
         <div>
             <div className="mt-3 ml-10 container">
@@ -78,7 +78,9 @@ export function CrearEmpleado() {
     </div>*/}
 
                     <div className="pt-1 mb-3 text-center">
-                        <button className="btn btn-guardar btn-lg btn-block">Guardar</button>
+                        <button className="btn btn-guardar btn-lg btn-block">
+                            Guardar
+                        </button>
                     </div>
                 </form>
             </div >
