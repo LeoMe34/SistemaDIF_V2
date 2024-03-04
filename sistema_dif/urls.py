02364a631781e49b1ae0e1128fe483621000ea6d
Mayreli Romero <may70981@gmail.com>
2024-03-04 14:06:07 -0600

    Login, back

---
+++ sistema_dif/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from knox import views as knox_views
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_api),
+    path('usuario/', views.get_datos_usuario),
+    path('registrar/', views.registrar_api),
+    path('logout/', knox_views.LogoutView.as_view()),
+    path('logoutall/', knox_views.LogoutAllView.as_view())
+]
--- sistema_dif/views.py
+++ sistema_dif/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
+from .serializers import RegistroSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def login_api(request):
+    # Crear una instancia del serializer AuthTokenSerializer con los datos de la solicitud
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    # Obtener el usuario autenticado a partir de los datos validados del serializer
+    user = serializer.validated_data['user']
+
+    # Crear un token para el usuario y agregarlo al diccionario que se enviará como respuesta
+    _, token = AuthToken.objects.create(user)
+
+    return Response({
+        'user_info': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        },
+        'token': token
+    })
+
+
+@api_view(['GET'])
+def get_datos_usuario(request):
+    # Obtener el usuario de la solicitud
+    user = request.user
+
+    # Verificar si el usuario esta autenticadp
+    if user.is_authenticated:
+        return Response({
+            'user_info': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email
+            },
+        })
+    # Si no está autenticado, retornar error 400
+    return Response({'error': 'no autenticado'}, status=400)
+
+
+@api_view(['POST'])
+def registrar_api(request):
+    #Crea una instancia del serializer RegistroSerializer con los datos de la solicitud
+    serializer = RegistroSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    
+    #Guarda al usuario utilizando el serializer
+    user = serializer.save()
+    
+    # Crear un nuevo token de autenticación para el usuario registrado utilizando Knox
+    # Esto haria como si se  hubiera logueado por primera vez
+    #_, token = AuthToken.objects.create(user)
+
+    return Response({
+        'user_info': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        },
+        #'token': token
+    })
