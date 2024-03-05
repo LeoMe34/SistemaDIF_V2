049e7ecd09eb3e02d9021bd880eab5036540b982
Mayreli Romero <may70981@gmail.com>
2024-03-04 22:17:55 -0600

    Ya se puede hacer el login desde el front

--- front/src/Components/Sesion/Login.jsx
+++ front/src/Components/Sesion/Login.jsx
@@ -1,5 +1,38 @@
+import axios from 'axios';
+import { useForm } from "react-hook-form"
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom"
+
 
 export function Login() {
+
+    const navegador = useNavigate()
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm()
+
+    const iniciarSesion = async () => {
+        try {
+            const url = "http://127.0.0.1:8000/api/login/"
+
+            const resultado = await axios.post(url, {
+                username: usuario,
+                password: contrasenia
+            })
+            setUsuario(resultado.data)
+            console.log("Usuario logueado correctamente")
+        } catch (error) {
+            console.error("Ocurrio un error", error)
+        }
+
+    }
+    const enviar = handleSubmit(async data => {
+        try {
+            await iniciarSesion
+            navegador("/home_enfermeria")
+        } catch (error) {
+            console.error("Error al crear empleado", error);
+        }
+    })
+
     return (
         <section className="vh-100" style={{ background: '#812A71' }}>
             <div className="container py-4 h-100">
@@ -26,20 +59,25 @@ export function Login() {
                                             <h5 className="etiquetas mb-2 pb-3" style={{ letterSpacing: '1px' }}>Iniciar sesión</h5>
 
                                             <div className="form-outline mb-3">
-                                                <input type="no_trabajador" id="no_trabajador" className="form-control form-control-lg entrada" placeholder="MRR009" />
+                                                <input type="no_trabajador" id="no_trabajador" className="form-control form-control-lg entrada"
+                                                    placeholder="MRR009" {...register("username", { required: true })} />
+                                                {errors.username && <span>Es necesario este campo</span>}
                                                 <label className="form-label etiquetas" htmlFor="form2Example17">Número de trabajador</label>
                                             </div>
 
                                             <div className="form-outline mb-3">
                                                 <div className="input-group">
-                                                    <input type="password" id="contrasenia" className="form-control form-control-lg entrada" placeholder="********" />
+                                                    <input type="password" id="contrasenia" className="form-control form-control-lg entrada"
+                                                        placeholder="********" autoComplete="new-password"
+                                                        {...register("password", { required: true })} />
+                                                    {errors.password && <span>Es necesario este campo</span>}
                                                 </div>
                                                 <label className="form-label etiquetas" htmlFor="contrasenia">Contraseña</label>
                                             </div>
 
                                             {/*Editar el botón */}
                                             <div className="pt-1 mb-3">
-                                                <button className="btn btn-guardar btn-lg btn-block">Entrar</button>
+                                                <button className="btn btn-guardar btn-lg btn-block" type="submit" onClick={enviar}>Entrar</button>
                                             </div>
                                             <a className=" etiquetas text-muted" href="#!" style={{ display: 'block' }}>¿Olvidaste tu contraseña?</a>
 
