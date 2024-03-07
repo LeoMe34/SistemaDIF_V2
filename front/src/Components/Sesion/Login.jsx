4231be7bf8236f0af30d8d49651eacf1b04b3e45
Mayreli Romero <may70981@gmail.com>
2024-03-07 11:08:53 -0600

    Funciona login,no valida si ya tiene una sesionAct

--- front/src/Components/Sesion/Login.jsx
+++ front/src/Components/Sesion/Login.jsx
@@ -7,6 +7,7 @@ export function Login() {
     const [usuario, setUsuario] = useState([])
     const [contrasenia, setContrasenia] = useState('')
     const navegador = useNavigate()
+    const source = axios.CancelToken.source()
 
     const iniciarSesion = async () => {
         try {
@@ -14,7 +15,8 @@ export function Login() {
             const respuesta = await axios.post(url, {
                 username: usuario,
                 password: contrasenia
-            })
+            }, { cancelToken: source.token })
+
             const datosUsuario = respuesta.data.user_info;
 
             if ('username' in datosUsuario) {
@@ -44,63 +46,74 @@ export function Login() {
         }
     }
 
-    return (
-        <section className="vh-100" style={{ background: '#812A71' }}>
-            <div className="container py-4 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
-                        <div className="card" >
-                            <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="../Logos/LOGO DIF.jpeg " alt="login form"
-                                        className="img-fluid" style={{
-                                            marginTop: '100px', marginLeft: 'auto', marginRight: 'auto', display: 'block',
-                                        }} />
-                                </div>
-                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-black">
+    useEffect(() => {
+        return () => {
+            source.cancel('La solicitud fue cancelada porque el componente se desmontó.');  // Cancela
+        }
+    })
 
-                                        <form>
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        iniciarSesion()
+    }
 
-                                            <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
-                                                <span className="h1 Titulo mb-0">Desarrollo Integral de la Familia</span>
-                                            </div>
+    return (        
+            <section className="vh-100" style={{ background: '#812A71' }}>
+                <div className="container py-4 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col col-xl-10">
+                            <div className="card" >
+                                <div className="row g-0">
+                                    <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                        <img src="../Logos/LOGO DIF.jpeg " alt="login form"
+                                            className="img-fluid" style={{
+                                                marginTop: '100px', marginLeft: 'auto', marginRight: 'auto', display: 'block',
+                                            }} />
+                                    </div>
+                                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                        <div className="card-body p-4 p-lg-5 text-black">
 
-                                            <h5 className="etiquetas mb-2 pb-3" style={{ letterSpacing: '1px' }}>Iniciar sesión</h5>
+                                            <form onSubmit={handleSubmit}>
 
-                                            <div className="form-outline mb-3">
-                                                <input type="text" id="username" className="form-control form-control-lg entrada"
-                                                    placeholder="MRR009" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
-                                                <label className="form-label etiquetas" htmlFor="form2Example17">Número de trabajador</label>
-                                            </div>
-
-                                            <div className="form-outline mb-3">
-                                                <div className="input-group">
-                                                    <input type="password" id="contrasenia" className="form-control form-control-lg entrada"
-                                                        placeholder="********" value={contrasenia} autoComplete="new-password"
-                                                        onChange={(e) => setContrasenia(e.target.value)} />
+                                                <div className="d-flex align-items-center mb-3 pb-1">
+                                                    <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                                                    <span className="h1 Titulo mb-0">Desarrollo Integral de la Familia</span>
                                                 </div>
-                                                <label className="form-label etiquetas" htmlFor="contrasenia">Contraseña</label>
-                                            </div>
 
-                                            {/*Editar el botón */}
-                                            <div className="pt-1 mb-3">
-                                                <button className="btn btn-guardar btn-lg btn-block" onClick={iniciarSesion}>Entrar</button>
-                                            </div>
-                                            <a className=" etiquetas text-muted" href="#!" style={{ display: 'block' }}>¿Olvidaste tu contraseña?</a>
+                                                <h5 className="etiquetas mb-2 pb-3" style={{ letterSpacing: '1px' }}>Iniciar sesión</h5>
 
-                                            <a href="#!" className=" small etiquetas text-muted" style={{ display: 'block' }}>Terminos y condiciones </a>
-                                            <a href="#!" className=" small etiquetas text-muted" style={{ display: 'block' }}>Política de privacidad</a>
-                                        </form>
+                                                <div className="form-outline mb-3">
+                                                    <input type="text" id="username" className="form-control form-control-lg entrada"
+                                                        placeholder="MRR009" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+                                                    <label className="form-label etiquetas" htmlFor="form2Example17">Número de trabajador</label>
+                                                </div>
 
+                                                <div className="form-outline mb-3">
+                                                    <div className="input-group">
+                                                        <input type="password" id="contrasenia" className="form-control form-control-lg entrada"
+                                                            placeholder="********" value={contrasenia} autoComplete="new-password"
+                                                            onChange={(e) => setContrasenia(e.target.value)} />
+                                                    </div>
+                                                    <label className="form-label etiquetas" htmlFor="contrasenia">Contraseña</label>
+                                                </div>
+
+                                                {/*Editar el botón */}
+                                                <div className="pt-1 mb-3">
+                                                    <button className="btn btn-guardar btn-lg btn-block" type='submit'>Entrar</button>
+                                                </div>
+                                                <a className=" etiquetas text-muted" href="#!" style={{ display: 'block' }}>¿Olvidaste tu contraseña?</a>
+
+                                                <a href="#!" className=" small etiquetas text-muted" style={{ display: 'block' }}>Terminos y condiciones </a>
+                                                <a href="#!" className=" small etiquetas text-muted" style={{ display: 'block' }}>Política de privacidad</a>
+                                            </form>
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>        
     )
 }
