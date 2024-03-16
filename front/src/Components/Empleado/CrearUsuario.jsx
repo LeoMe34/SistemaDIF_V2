8f3b9eeaa263b76840dea983af7f65fc99582059
Mayreli Romero <may70981@gmail.com>
2024-03-15 21:45:48 -0600

    Utilizacion de la api crear usuario

--- front/src/Components/Empleado/CrearUsuario.jsx
+++ front/src/Components/Empleado/CrearUsuario.jsx
@@ -1,5 +1,46 @@
 {/*import { NavBarSimple } from "../../Partials/NavBarSimple"*/ }
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom"
+
 export function CrearUsuario() {
+    const [usuario, setUsuario] = useState({ username: '', password: '', email: '' });
+    const navegador = useNavigate();
+
+    const registrarUsuario = async () => {
+        try {
+            const url = "http://127.0.0.1:8000/api/registrar/";
+            const respuesta = await axios.post(url, {
+                username: usuario.username,
+                password: usuario.password,
+                email: usuario.email
+            });
+            const datosUsuario = respuesta.data.user_info;
+            if ('username' in datosUsuario) {
+                console.log("Usuario duplicado");
+                // Aquí puedes manejar el caso de usuario duplicado
+            } else {
+                console.log("Usuario registrado correctamente");
+                navegador("/crear_empleado");
+            }
+        } catch (error) {
+            console.error("Ocurrió un error", error);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        registrarUsuario();
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUsuario(prevUsuario => ({
+            ...prevUsuario,
+            [name]: value
+        }));
+    };
+
     return (
         <div>
             <div className="mt-3 ml-10 container">
@@ -22,30 +63,33 @@ export function CrearUsuario() {
 
             {/*<NavBarSimple />*/}
             <div className="ml-10 container mt-3">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h3 className="titulo">Registrar usuario</h3>
                     <div className='row'>
                         <div className='mt-2 mb-2 col'>
-                            <label className="etiqueta" htmlFor="noTrabajador">Número de trabajador</label>
-                            <input id="noTrabajador" type="text" placeholder="No. de trabajador" className="entrada" />
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='mt-2 mb-2 col'>
                             <label className="etiqueta" htmlFor="usuario">Usuario</label>
-                            <input id="usuario" type="text" className="entrada" readOnly />
+                            <input id="usuario" type="text" className="entrada"
+                                name="username"
+                                value={usuario.username}
+                                onChange={handleChange} />
                         </div>
                     </div>
                     <div className='row'>
                         <div className='mt-2 mb-2 col'>
                             <label className="etiqueta" htmlFor="email">Correo electrónico</label>
-                            <input id="email" type="text" placeholder="Correo electrónico" className="entrada" />
+                            <input id="email" type="text" placeholder="Correo electrónico" className="entrada"
+                                name="email"
+                                value={usuario.email}
+                                onChange={handleChange} />
                         </div>
                     </div>
                     <div className='row'>
                         <div className='mt-2 mb-2 col'>
                             <label className="etiqueta" htmlFor="contrasenia">Contraseña</label>
-                            <input id="contrasenia" type="password" placeholder="Contraseña" className="entrada" />
+                            <input id="contrasenia" type="password" placeholder="Contraseña" className="entrada"
+                                name="password"
+                                value={usuario.password}
+                                onChange={handleChange} />
                         </div>
                     </div>
 
--- sistema_dif/urls.py
+++ sistema_dif/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('usuario/', views.get_datos_usuario),
     path('registrar/', views.registrar_api),
     path('logout/', knox_views.LogoutView.as_view()),
-    path('logoutall/', knox_views.LogoutAllView.as_view())
+    path('logoutall/', knox_views.LogoutAllView.as_view()),
+
+    path('registrar_empleado', views.crear_empleado)
 ]
