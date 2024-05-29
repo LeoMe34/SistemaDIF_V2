9297484d3858dea6d0907218dbfd190ea19186b6
Mayreli Romero <may70981@gmail.com>
2024-05-29 17:49:07 -0600

    Validacion de fichaTecnicaEnfermeria

--- front/src/Components/FormatoEnfermeria/FichaTecnicaEnfermeria.jsx
+++ front/src/Components/FormatoEnfermeria/FichaTecnicaEnfermeria.jsx
@@ -5,13 +5,12 @@ import axios from 'axios';
 import { useForm } from "react-hook-form"
 import BusquedaPaciente from "../Paciente/BuscarPaciente"
 import { useNoExpediente } from '../../Contexto/NoExpedienteContext';
-{/*import { NavBarSimple } from "../../Partials/NavBarSimple"*/ }
-
+import { toast } from 'react-hot-toast'
 
 export function FichaTecnicaEnfermeria() {
     const navegador = useNavigate()
     const { token } = useAuth()
-    const { register, handleSubmit, formState: { errors }, setValue } = useForm()
+    const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm()
     const { noExpediente } = useNoExpediente()
     const [noEmpleado, setNoEmpleado] = useState(null);
     const [nombreE, setNombreE] = useState(null);
@@ -37,6 +36,20 @@ export function FichaTecnicaEnfermeria() {
 
         getNoEmpleado();
     }, [token]);
+
+    useEffect(() => {
+        const today = new Date().toISOString().split('T')[0];
+        setValue("fecha", today);
+    }, [setValue]);
+
+    useEffect(() => {
+        const peso = watch('peso');
+        const talla = watch('talla');
+        if (peso && talla) {
+            const imc = (parseFloat(peso) / (parseFloat(talla) ** 2)).toFixed(2);
+            setValue("imc", imc);
+        }
+    }, [watch(['peso', 'talla']), setValue]);
 
     const registrarFicha = async (data) => {
         try {
@@ -78,9 +91,74 @@ export function FichaTecnicaEnfermeria() {
         }
     }
 
+    const validarPesoYTalla = (pesoYTalla) => {
+        const pesoYTallaRegex = /^[0-9.]{1,10}$/
+
+        return pesoYTallaRegex.test(pesoYTalla)
+    }
+
+    const validarPresionYFrecCardioYFrecRespYglicemia = (presionYfcYfrYg) => {
+        const presionRegex = /^[0-9/\s]{1,7}$/
+
+        return presionRegex.test(presionYfcYfrYg)
+    }
+
+    const validarTemperatura = (temperatura) => {
+        const temperaturaRegex = /^[0-9.]{1,5}$/
+
+        return temperaturaRegex.test(temperatura)
+    }
+
+    const validarReligionYEscolaridad = (religionYEscolar) => {
+        const religionYEscolaridadRegex = /^[A-Za-zÁÉÍÓÚáéíóúü\s]{1,50}$/
+
+        return religionYEscolaridadRegex.test(religionYEscolar)
+    }
+
     const enviar = handleSubmit(async data => {
-        registrarFicha(data)
+        const pesoValido = validarPesoYTalla(data.peso)
+        const tallaValido = validarPesoYTalla(data.talla)
+        const presionValido = validarPresionYFrecCardioYFrecRespYglicemia(data.presion)
+        const frecCardioValido = validarPresionYFrecCardioYFrecRespYglicemia(data.frecuencia_cardiaca)
+        const frecRespValido = validarPresionYFrecCardioYFrecRespYglicemia(data.frecuencia_respiratoria)
+        const glicemiaValido = validarPresionYFrecCardioYFrecRespYglicemia(data.glicemia)
+        const temperaturaValido = validarTemperatura(data.temperatura)
+        const religionValido = validarReligionYEscolaridad(data.religion)
+        const escolaridadValido = validarReligionYEscolaridad(data.escolaridad)
+
+        if (!pesoValido) {
+            toast.error("Ingrese sólo números y . en el peso")
+        } else if (!tallaValido) {
+            toast.error("Ingrese sólo números y . en la talla")
+        } else if (!presionValido) {
+            toast.error("Ingrese sólo números y / en el campo del T/A")
+        } else if (!frecCardioValido) {
+            toast.error("Ingrese sólo números y / en la frecuencia cardiaca")
+        } else if (!frecRespValido) {
+            toast.error("Ingrese sólo números y / en la frecuencia respiratoria")
+        } else if (!glicemiaValido) {
+            toast.error("Ingrese sólo números y / en el campo de glicemia")
+        } else if (!temperaturaValido) {
+            toast.error("Ingrese sólo números y . en el campo de temperatura")
+        } else if (!religionValido) {
+            toast.error("Ingrese sólo letras en el campo de religion")
+        } else if (!escolaridadValido) {
+            toast.error("Ingrese sólo letras en el campo de escolaridad")
+        }
+        else {
+            registrarFicha(data)
+        }
     })
+
+    const handleCheckboxChange = (e) => {
+        if (e.target.name === "ninguna") {
+            setValue("discapacitado", false);
+            setValue("adulto_mayor", false);
+            setValue("embarazada", false);
+        } else {
+            setValue("ninguna", false);
+        }
+    };
 
     return (
         <div>
@@ -106,17 +184,20 @@ export function FichaTecnicaEnfermeria() {
                         <div className='col'>
                             <label className='etiqueta' htmlFor="fecha">Fecha: </label>
                             <input className="entrada" id='fecha' name='fecha' type="date"
-                                {...register("fecha", { required: true })} />
+                                {...register("fecha", { required: true })} readOnly />
                         </div>
 
                         <div className='col'>
-                            <label className='etiqueta' htmlFor="es_trabajador">Trabajador: </label>
+                            <label className='etiqueta' htmlFor="es_trabajador">Trabajador:
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <select className="opciones" id='es_trabajador' name='es_trabajador' type=""
                                 {...register("es_trabajador", { required: true })}>
                                 <option value="" selected disabled>Elija una opción</option>
                                 <option value={true}>Si</option>
                                 <option value={false}>No</option>
                             </select>
+                            {errors.es_trabajador && <span>Es necesario este campo</span>}
                         </div>
 
                     </div>
@@ -125,20 +206,29 @@ export function FichaTecnicaEnfermeria() {
                 <div className='ml-10 mt-2 container'>
                     <div className='row'>
                         <div className='col'>
-                            <label className='etiqueta' htmlFor="peso">Peso: </label>
+                            <label className='etiqueta' htmlFor="peso">Peso (Kg):
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <input className="entrada" id='peso' name='peso' type="text"
                                 {...register("peso", { required: true })} />
+                            {errors.peso && <span>Es necesario este campo</span>}
                         </div>
                         <div className='col'>
-                            <label className='etiqueta' htmlFor="talla">Talla: </label>
+                            <label className='etiqueta' htmlFor="talla">Talla (m):
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <input className="entrada" id='talla' name='talla' type="text"
                                 {...register("talla", { required: true })} />
+                            {errors.talla && <span>Es necesario este campo</span>}
                         </div>
 
                         <div className='col'>
-                            <label className='etiqueta' htmlFor="imc">IMC: </label>
+                            <label className='etiqueta' htmlFor="imc">IMC:
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <input className="entrada" id='imc' name='imc' type="text"
-                                {...register("imc", { required: true })} />
+                                {...register("imc", { required: true })} readOnly />
+                            {errors.imc && <span>Es necesario este campo</span>}
                         </div>
                     </div>
                 </div>
@@ -149,7 +239,9 @@ export function FichaTecnicaEnfermeria() {
                     <div className='ml-10 mb-5 container'>
                         <div className='row'>
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="tipo_atencion">Tipo de atención</label>
+                                <label className='etiqueta' htmlFor="tipo_atencion">Tipo de atención
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <select className="opciones" id='tipo_atencion' name='tipo_atencion' type=""
                                     {...register("tipo_atencion", { required: true })}>
                                     <option value="" selected disabled>Elija una opción</option>
@@ -159,17 +251,24 @@ export function FichaTecnicaEnfermeria() {
                                     <option value="4">Aplicación de medicamentos</option>
                                     <option value="5">DxTx</option>
                                 </select>
+                                {errors.tipo_atencion && <span>Es necesario este campo</span>}
                             </div>
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="presion">T/A: </label>
+                                <label className='etiqueta' htmlFor="presion">T/A:
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='presion' name='presion' type="text"
                                     {...register("presion", { required: true })} />
+                                {errors.presion && <span>Es necesario este campo</span>}
                             </div>
 
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="frecuencia_cardiaca">Frecuencia cardiaca: </label>
+                                <label className='etiqueta' htmlFor="frecuencia_cardiaca">Frecuencia cardiaca (lpm):
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='frecuencia_cardiaca' name='frecuencia_cardiaca' type="text"
                                     {...register("frecuencia_cardiaca", { required: true })} />
+                                {errors.frecuencia_cardiaca && <span>Es necesario este campo</span>}
                             </div>
                         </div>
                     </div>
@@ -177,21 +276,30 @@ export function FichaTecnicaEnfermeria() {
                     <div className='ml-10 mb-5 container'>
                         <div className='row'>
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="frecuencia_respiratoria">Frecuencia respiratoria: </label>
+                                <label className='etiqueta' htmlFor="frecuencia_respiratoria">Frecuencia respiratoria (rpm):
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='frecuencia_respiratoria' name='frecuencia_respiratoria' type="text"
                                     {...register("frecuencia_respiratoria", { required: true })} />
+                                {errors.frecuencia_respiratoria && <span>Es necesario este campo</span>}
                             </div>
 
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="temperatura">Temperatura: </label>
+                                <label className='etiqueta' htmlFor="temperatura">Temperatura:
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='temperatura' name='temperatura' type="text"
                                     {...register("temperatura", { required: true })} />
+                                {errors.temperatura && <span>Es necesario este campo</span>}
                             </div>
 
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="glicemia">Glicemia capilar: </label>
+                                <label className='etiqueta' htmlFor="glicemia">Glicemia capilar:
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='glicemia' name='glicemia' type="text"
                                     {...register("glicemia", { required: true })} />
+                                {errors.glicemia && <span>Es necesario este campo</span>}
                             </div>
                         </div>
                     </div>
@@ -199,40 +307,48 @@ export function FichaTecnicaEnfermeria() {
                     <div className='ml-10 mb-5 container'>
                         <div className='row'>
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="religion">Religión: </label>
+                                <label className='etiqueta' htmlFor="religion">Religión:
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='religion' name='religion' type="text"
                                     {...register("religion", { required: true })} />
+                                {errors.religion && <span>Es necesario este campo</span>}
                             </div>
 
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="escolaridad">Escolaridad: </label>
+                                <label className='etiqueta' htmlFor="escolaridad">Escolaridad:
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
                                 <input className="entrada" id='escolaridad' name='escolaridad' type="text"
                                     {...register("escolaridad", { required: true })} />
+                                {errors.escolaridad && <span>Es necesario este campo</span>}
                             </div>
 
                             <div className='col'>
-                                <label className='etiqueta' htmlFor="tipo_poblacion">Tipo de población</label>
+                                <label className='etiqueta' htmlFor="tipo_poblacion">Tipo de población
+                                    <span className='etiqueta_obligatoria'>*</span>
+                                </label>
 
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input caja_opciones" type="checkbox" id='discapacitado' name='discapacitado'
-                                        {...register("discapacitado")} />
+                                        {...register("discapacitado")} onChange={handleCheckboxChange} />
                                     <label className='form-check-label etiqueta' htmlFor="discapacitado">Discapacitado</label>
                                 </div>
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input caja_opciones" id='adulto_mayor' name='adulto_mayor' type="checkbox"
-                                        {...register("adulto_mayor")} />
+                                        {...register("adulto_mayor")} onChange={handleCheckboxChange} />
                                     <label className='form-check-label etiqueta' htmlFor="adulto_mayor">Adulto mayor</label>
                                 </div>
 
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input caja_opciones" id='embarazada' name='embarazada' type="checkbox"
-                                        {...register("embarazada")} />
+                                        {...register("embarazada")} onChange={handleCheckboxChange} />
                                     <label className='form-check-label etiqueta' htmlFor="embarazada">Embarazada</label>
                                 </div>
 
                                 <div className="form-check form-check-inline">
                                     <input className="form-check-input caja_opciones" id='ninguna' name='ninguna' type="checkbox"
-                                        {...register("ninguna")} />
+                                        {...register("ninguna")} onChange={handleCheckboxChange} />
                                     <label className='form-check-label etiqueta' htmlFor="ninguna">Ninguna</label>
                                 </div>
                             </div>
@@ -243,8 +359,8 @@ export function FichaTecnicaEnfermeria() {
                         <div className='row'>
                             <div className='col'>
                                 <label className='etiqueta' htmlFor="enfermero">Enfermero</label>
-                                <input className="datos_lectura" id='enfermero' name='enfermero' type="text" 
-                                value={nombreE} readOnly />
+                                <input className="datos_lectura" id='enfermero' name='enfermero' type="text"
+                                    value={nombreE} readOnly />
                             </div>
                         </div>
                     </div>
