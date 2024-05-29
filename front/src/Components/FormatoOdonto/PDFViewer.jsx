130904e3a8852d1db648b73ecb2b8da74218c2c3
leonardo <zS20019090@estudiantes.uv.mx>
2024-05-28 22:12:19 -0600

    PDF MostrarHistClOdonto ya se puede

---
+++ front/src/Components/FormatoOdonto/PDFViewer.jsx
@@ -0,0 +1,45 @@
+import { Document, Page } from 'react-pdf';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function PDFViewer({ detalles, token }) {
+    const [pdfData, setPdfData] = useState(null);
+
+    useEffect(() => {
+        const getPdfData = async () => {
+            try {
+                const response = await axios.get(`http://127.0.0.1:8000${detalles.archivo}`, {
+                    responseType: 'arraybuffer',
+                    headers: {
+                        Authorization: `Token ${token}`
+                    }
+                });
+                const blob = new Blob([response.data], { type: 'application/pdf' });
+                setPdfData(blob);
+            } catch (error) {
+                console.error('Error al obtener el PDF', error);
+            }
+        };
+
+        getPdfData();
+    }, [detalles, token]);
+
+    return (
+        <div>
+            {pdfData && (
+                <iframe
+                    src={URL.createObjectURL(pdfData)}
+                    width="100%"
+                    height="600px"
+                    title="Documento PDF"
+                    frameBorder="0"
+                    allowFullScreen
+                />
+            )}
+        </div>
+    );
+}
+
+export default PDFViewer;
+
