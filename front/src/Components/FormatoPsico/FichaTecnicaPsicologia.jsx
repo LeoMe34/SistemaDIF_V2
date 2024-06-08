99f376470dcc9a46667353a6381c447e90269460
Mayreli Romero <may70981@gmail.com>
2024-06-08 12:43:47 -0600

    Validacion fichaTecnicaPsicologia

--- front/src/Components/FormatoPsico/FichaTecnicaPsicologia.jsx
+++ front/src/Components/FormatoPsico/FichaTecnicaPsicologia.jsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form"
 import { useState, useEffect } from 'react';
 import { useNoExpediente } from '../../Contexto/NoExpedienteContext';
 import BuscarPacientePsico from '../Paciente/BuscarPacientePsico'
-
+import { toast } from 'react-hot-toast'
 
 export function FichaTecnicaPsicologia() {
     const navegador = useNavigate()
@@ -14,6 +14,7 @@ export function FichaTecnicaPsicologia() {
     const { noExpediente } = useNoExpediente()
     const [noEmpleado, setNoEmpleado] = useState(null);
     const [empleado, setEmpleado] = useState([]);
+    const [fechaActual, setFechaActual] = useState('')
 
     useEffect(() => {
         const getNoEmpleado = async () => {
@@ -35,6 +36,15 @@ export function FichaTecnicaPsicologia() {
 
         getNoEmpleado();
     }, [token]);
+
+    useEffect(() => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+        setFechaActual(formattedDate);
+    }, []);
 
     const registrarFicha = async (data) => {
         try {
@@ -71,8 +81,60 @@ export function FichaTecnicaPsicologia() {
         }
     }
 
+    const validarEscolaridad = (escolar) => {
+        const escolarRegex = /^[A-Za-zÁÉÍÓÚáéíóúü0-9\s.-]{1,80}$/
+
+        return escolarRegex.test(escolar)
+    }
+
+    const validarAcompanianteYParentesco = (acompanianteyParentesco) => {
+        const acompanianteyParentescoRegex = /^[A-Za-zÁÉÍÓÚáéíóúü\s]{1,80}$/
+
+        return acompanianteyParentescoRegex.test(acompanianteyParentesco)
+    }
+
+    const validarTexto = (entrada) => {
+        const entradaRegex = /^[A-Za-zÁÉÍÓÚáéíóúü0-9\s.-:,;()/]{1,10000}$/
+
+        return entradaRegex.test(entrada)
+    }
+
     const enviar = handleSubmit(async data => {
-        registrarFicha(data)
+        const escolaridadValido = validarEscolaridad(data.escolaridad)
+        const acompaniaValido = validarAcompanianteYParentesco(data.acompania)
+        const parentescoValido = validarAcompanianteYParentesco(data.parentesco)
+        const motivoValido = validarTexto(data.motivo_consulta)
+        const antecedenteValido = validarTexto(data.antecedentes)
+        const impresionValido = validarTexto(data.impresion_diagnostica)
+        const tratamientoValido = validarTexto(data.tratamiento)
+        const sugerenciaValido = validarTexto(data.sugerencias)
+        const valoracionValido = validarTexto(data.valoracion)
+        if (!escolaridadValido) {
+            toast.error("En el campo de parentesco solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-");
+        } else if (!acompaniaValido) {
+            toast.error("En el campo de acompañante solo se puede ingresar caracteres alfabeticos");
+        } else if (!parentescoValido) {
+            toast.error("En el campo de parentesco solo se puede ingresar caracteres alfabeticos");
+        } else if (!motivoValido) {
+            toast.error("En el campo de motivo de consulta solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-:,;()/");
+        } else if (!antecedenteValido) {
+            toast.error("En el campo de antecedentes solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-:,;()/");
+        } else if (!impresionValido) {
+            toast.error("En el campo de impresión diagnóstica solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-:,;()/");
+        } else if (!tratamientoValido) {
+            toast.error("En el campo de tratamiento solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-:,;()/");
+        } else if (!sugerenciaValido) {
+            toast.error("En el campo de sugerencias y recomendaciones solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-:,;()/");
+        } else if (!valoracionValido) {
+            toast.error("En el campo de valoración PE solo se puede ingresar caracteres alfanuméricos y signos de puntuación como: .-:,;()/");
+        } else if (noExpediente === null) {
+            toast.error("Debe seleccionar un paciente");
+        }
+        else {
+            registrarFicha(data)
+            navegador("/home_psicologia")
+        }
+
     })
 
     return (
@@ -101,13 +163,16 @@ export function FichaTecnicaPsicologia() {
                 <form onSubmit={enviar}>
                     <div className='row'>
                         <div className=" col">
-                            <label className=' etiqueta' htmlFor="fecha">Fecha: </label>
+                            <label className=' etiqueta' htmlFor="fecha">Fecha </label>
                             <input className="entrada" id='fecha' name='fecha' type="date"
+                                value={fechaActual} readOnly
                                 {...register("fecha", { required: true })} />
                         </div>
 
                         <div className="mt-2 col">
-                            <label className=' etiqueta' htmlFor="tipo_cons">Tipo de consulta: </label>
+                            <label className=' etiqueta' htmlFor="tipo_cons">Tipo de consulta
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <select className="opciones" id='tipo_cons' name='tipo_cons' type=""
                                 {...register("tipo_cons", { required: true })}>
                                 <option value="" selected disabled>Elija una opción</option>
@@ -127,94 +192,126 @@ export function FichaTecnicaPsicologia() {
                                 <option value="14">Terapia grupal</option>
                                 <option value="15">Otro</option>
                             </select>
+                            {errors.tipo_cons && <span>Es necesario este campo</span>}
                         </div>
 
                         <div className="col">
-                            <label className='mt-2 etiqueta' htmlFor="visita">Visita: </label>
+                            <label className='mt-2 etiqueta' htmlFor="visita">Visita
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <select className="opciones" id='visita' name='visita' type=""
                                 {...register("visita", { required: true })}>
                                 <option value="" selected disabled>Elija una opción</option>
                                 <option value="1">Primera vez</option>
                                 <option value="2">Subsecuente</option>
                             </select>
+                            {errors.visita && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-2 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="escolaridad">Escolaridad:</label>
+                            <label className="etiqueta" htmlFor="escolaridad">Escolaridad
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <input className="entrada" id='escolaridad' name='escolaridad' type="text"
                                 {...register("escolaridad", { required: true })} />
+                            {errors.escolaridad && <span>Es necesario este campo</span>}
                         </div>
+
                         <div className="col">
-                            <label className="etiqueta" htmlFor="acompania">Acompañante:</label>
+                            <label className="etiqueta" htmlFor="acompania">Acompañante
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <input className="entrada" id='acompania' name='acompania' type="text"
                                 {...register("acompania", { required: true })} />
+                            {errors.acompania && <span>Es necesario este campo</span>}
                         </div>
                         <div className="col">
-                            <label className="etiqueta" htmlFor="parentesco">Parentesco:</label>
+                            <label className="etiqueta" htmlFor="parentesco">Parentesco
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <input className="entrada" id='parentesco' name='parentesco' type="text"
                                 {...register("parentesco", { required: true })} />
+                            {errors.parentesco && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <h3 className="subtitulo_2">Datos del paciente</h3>
                     <div className="mt-2 mb-3 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="motivo_consulta">Motivo de consulta</label>
+                            <label className="etiqueta" htmlFor="motivo_consulta">Motivo de consulta
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <textarea id="motivo_consulta" placeholder="..." className="text-amplio"
                                 {...register("motivo_consulta", { required: true })} />
+                            {errors.motivo_consulta && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-2 mb-3 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="antecedentes">Antecedentes</label>
+                            <label className="etiqueta" htmlFor="antecedentes">Antecedentes
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <textarea id="antecedentes" placeholder="..." className="text-amplio"
                                 {...register("antecedentes", { required: true })} />
+                            {errors.antecedentes && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-2 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="impresion_diagnostica">Impresión diagnóstica</label>
+                            <label className="etiqueta" htmlFor="impresion_diagnostica">Impresión diagnóstica
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <textarea id="impresion_diagnostica" placeholder="..." className="text-amplio"
                                 {...register("impresion_diagnostica", { required: true })} />
+                            {errors.impresion_diagnostica && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-2 mb-3 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="tratamiento">Tratamiento:</label>
+                            <label className="etiqueta" htmlFor="tratamiento">Tratamiento
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <textarea id="tratamiento" placeholder="..." className="text-amplio"
                                 {...register("tratamiento", { required: true })} />
+                            {errors.tratamiento && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-2 mb-3 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="sugerencias">Sugerencias y recomendaciones:</label>
+                            <label className="etiqueta" htmlFor="sugerencias">Sugerencias y recomendaciones
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <textarea id="sugerencias" placeholder="..." className="text-amplio"
                                 {...register("sugerencias", { required: true })} />
+                            {errors.sugerencias && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-2 mb-3 row">
                         <div className="col">
-                            <label className="etiqueta" htmlFor="valoracion">Valoración PE:</label>
+                            <label className="etiqueta" htmlFor="valoracion">Valoración PE
+                                <span className='etiqueta_obligatoria'>*</span>
+                            </label>
                             <textarea id="valoracion" placeholder="..." className="text-amplio"
                                 {...register("valoracion", { required: true })} />
+                            {errors.valoracion && <span>Es necesario este campo</span>}
                         </div>
                     </div>
 
                     <div className="mt-5 mb-4 text-center">
                         <label className="etiqueta" htmlFor="psicologo">Psicólogo(A)</label>
-                        <input className="datos_lectura" id='psicologo' name='psicologo' type="text" 
-                        value={empleado.nombre_empleado} readOnly />
+                        <input className="datos_lectura" id='psicologo' name='psicologo' type="text"
+                            value={empleado.nombre_empleado} readOnly />
 
                         <label className="etiqueta" htmlFor="cedula_psi">Cédula</label>
-                        <input className="datos_lectura" id='cedula_psi' name='cedula_psi' type="text" 
-                        value={empleado.cedula} readOnly />
+                        <input className="datos_lectura" id='cedula_psi' name='cedula_psi' type="text"
+                            value={empleado.cedula} readOnly />
                     </div>
 
                     {/*Seccion del boton*/}
