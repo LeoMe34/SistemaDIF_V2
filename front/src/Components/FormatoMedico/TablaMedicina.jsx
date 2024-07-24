caaa7f66941b210ca6e5375f0bce9b883da0915d
Mayreli Romero <may70981@gmail.com>
2024-07-23 22:56:49 -0600

    PDF de hoja diaria de medicina

--- front/src/Components/FormatoMedico/TablaMedicina.jsx
+++ front/src/Components/FormatoMedico/TablaMedicina.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import { useAuth } from '../../Contexto/AuthContext';
 import BusquedaPaciente from "../Paciente/BuscarPaciente"
+import generarPDF from "./HojaDiariaPDF";
 
 export function TablaMedicina() {
     const { token } = useAuth()
@@ -121,6 +122,10 @@ export function TablaMedicina() {
                 <input type="text" id="cedula" name="cedula" className="entrada" value={cedula} readOnly />
                 <label className="etiqueta">Localidad sede: Coatzacoalcos</label>
 
+                <button className="ml-10 btn btn-guardar btn-lg btn-block"
+                    onClick={() => generarPDF(nombre, cedula, historialClinico, detallesPacientes, fechaActual)}>
+                    Descargar hoja diaria en PDF
+                </button>
 
                 <table className="mt-3 table table-bordered border-dark table-hover">
                     <thead className="">
