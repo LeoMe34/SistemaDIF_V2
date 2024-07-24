ffd7d6bf002167704f8d216402b209e5608fbf08
Mayreli Romero <may70981@gmail.com>
2024-07-24 14:30:28 -0600

    Excel de hoja diaria de nutricion

---
+++ front/src/Components/FormatoNutriologo/HojaDiariaExcel.jsx
@@ -0,0 +1,48 @@
+import { writeFile, utils } from 'xlsx';
+
+const convertirReferencia = (referencia, lugarReferencia) => {
+    if (referencia === "1") {
+        return lugarReferencia;
+    } else if (referencia === "2") {
+        return "No";
+    } else {
+        return ""; // Manejar otros casos si es necesario
+    }
+}
+
+const convertirVisita = (visita) => {
+    switch (visita) {
+        case "1":
+            return "Primera vez";
+        case "2":
+            return "Subsecuente";
+        default:
+            return "";
+    }
+}
+
+const generarExcel = (nombre, cedula, fichasMedicas, detallesPacientes, fechaActual) => {
+    const datos = [
+        ['Nombre del Médico:', nombre],         
+        ['Cédula Profesional:', cedula], 
+        [],
+        ['No.Expediente', 'Nombre', 'Visita', 'Referencia', 'Sexo', 'Edad', 'Diagnóstico'],
+        ...fichasMedicas.map((ficha, index) => [
+            ficha.paciente,
+            `${detallesPacientes[index]?.datosPersonalesPacient.nombre} ${detallesPacientes[index]?.datosPersonalesPacient.apellidoP} ${detallesPacientes[index]?.datosPersonalesPacient.apellidoM}`,
+            convertirVisita(ficha.extras.visita),
+            convertirReferencia(ficha.extras.referencia, ficha.extras.lugar_referencia),
+            detallesPacientes[index]?.datosPersonalesPacient.sexo,
+            detallesPacientes[index]?.datosPersonalesPacient.edad,
+            ficha.diagnostico
+        ])
+    ];
+
+    const worksheet = utils.aoa_to_sheet(datos);
+    const workbook = utils.book_new();
+    utils.book_append_sheet(workbook, worksheet, 'Hoja Diaria Audiología');
+
+    writeFile(workbook, `HOJA_DIARIA_AUDIOLOGIA_${nombre}_${fechaActual}.xlsx`);
+};
+
+export default generarExcel;
--- front/src/Components/FormatoNutriologo/TablaNutriologo.jsx
+++ front/src/Components/FormatoNutriologo/TablaNutriologo.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import { useAuth } from '../../Contexto/AuthContext';
 import generarPDF from "./HojaDiariaPDF";
+import generarExcel from "./HojaDiariaExcel";
 
 export function TablaNutriologia() {
     const { token } = useAuth()
@@ -120,6 +121,11 @@ export function TablaNutriologia() {
                 <button className="ml-10 btn btn-guardar btn-lg btn-block"
                     onClick={() => generarPDF(nombre, cedula, fichasMedicas, detallesPacientes, fechaActual)}>
                     Descargar hoja diaria en PDF
+                </button>
+
+                <button className="ml-10 btn btn-guardar btn-lg btn-block"
+                    onClick={() => generarExcel(nombre, cedula, fichasMedicas, detallesPacientes, fechaActual)}>
+                    Descargar hoja diaria en Excel
                 </button>
 
                 <table className="mt-3 table table-bordered border-dark table-hover">
