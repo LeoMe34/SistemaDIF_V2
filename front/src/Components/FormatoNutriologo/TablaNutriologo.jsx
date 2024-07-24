25820d0595949f999eb3c4bae57fcba780b2765c
Mayreli Romero <may70981@gmail.com>
2024-07-23 23:06:57 -0600

    PDF de hoja diaria de nutriologo

--- front/src/Components/FormatoNutriologo/TablaNutriologo.jsx
+++ front/src/Components/FormatoNutriologo/TablaNutriologo.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import { useAuth } from '../../Contexto/AuthContext';
+import generarPDF from "./HojaDiariaPDF";
 
 export function TablaNutriologia() {
     const { token } = useAuth()
@@ -115,6 +116,11 @@ export function TablaNutriologia() {
                 <input type="text" id="nombre_nutri" name="nombre_nutri" className="entrada" value={nombre} readOnly />
                 <label className="etiqueta" htmlFor="cedula">Cedula profesional</label>
                 <input type="text" id="cedula" name="cedula" className="entrada" value={cedula} readOnly />
+
+                <button className="ml-10 btn btn-guardar btn-lg btn-block"
+                    onClick={() => generarPDF(nombre, cedula, fichasMedicas, detallesPacientes, fechaActual)}>
+                    Descargar hoja diaria en PDF
+                </button>
 
                 <table className="mt-3 table table-bordered border-dark table-hover">
                     <thead className="">
