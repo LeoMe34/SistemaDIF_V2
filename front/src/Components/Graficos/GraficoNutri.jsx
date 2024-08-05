07178c13159bf14edbf4e10605c0d3acff143c04
Mayreli Romero <may70981@gmail.com>
2024-08-05 10:59:43 -0600

    Ajustes de espacios, nutri

--- front/src/Components/Graficos/GraficoNutri.jsx
+++ front/src/Components/Graficos/GraficoNutri.jsx
@@ -34,19 +34,19 @@ export function GraficoNutri() {
                         <button className='areaButton m-3'><TbNurse />Enfermeria</button>
                     </Link>
                     <Link to={"/graficas_odonto"}>
-                        <button className='areaButton'><FaTeeth /> Odontologia</button>
+                        <button className='areaButton m-3'><FaTeeth /> Odontologia</button>
                     </Link>
                     <Link to={"/graficas_med"}>
-                        <button className='areaButton'><FaUserDoctor />Medicina</button>
+                        <button className='areaButton m-3'><FaUserDoctor />Medicina</button>
                     </Link>
                     <Link to={"/graficas_oft"}>
-                        <button className='areaButton'>< SlEyeglass />Oftalmología</button>
+                        <button className='areaButton m-3'>< SlEyeglass />Oftalmología</button>
                     </Link>
                     <Link to={"/graficas_aud"}>
-                        <button className='areaButton'>< IoEarOutline />Audiología</button>
+                        <button className='areaButton m-3'>< IoEarOutline />Audiología</button>
                     </Link>
                     <Link to={"/graficas_psic"}>
-                        <button className='areaButton'>< RiPsychotherapyLine />Psicologia</button>
+                        <button className='areaButton m-3'>< RiPsychotherapyLine />Psicologia</button>
                     </Link>
                 </div>
             </div>
