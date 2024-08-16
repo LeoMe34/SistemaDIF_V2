d611a8951566c1050f26d31c94749602c1aa1fc3
Mayreli Romero <may70981@gmail.com>
2024-08-16 16:47:53 -0600

    Paginacion de la tabla de oftalmologia

--- front/src/Components/FormatoOftalmologo/TablaOftalmologo.jsx
+++ front/src/Components/FormatoOftalmologo/TablaOftalmologo.jsx
@@ -14,6 +14,9 @@ export function TablaOftalmologo() {
     const [fichasMedicas, setFichasMedicas] = useState([])
     const [fechaActual, setFechaActual] = useState('')
 
+    const [currentPage, setCurrentPage] = useState(1);
+    const fichasPorPagina = 10;
+
     const getNoEmpleado = async () => {
         try {
 
@@ -107,16 +110,26 @@ export function TablaOftalmologo() {
         getFichasMedicas()
     }, [token, noEmpleado]);
 
+    const indexOfLastFicha = currentPage * fichasPorPagina;
+    const indexOfFirstFicha = indexOfLastFicha - fichasPorPagina;
+    const fichasActuales = fichasMedicas.slice(indexOfFirstFicha, indexOfLastFicha);
+
+    // Cambiar de página
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
         <div className="container">
-            <div className="">
+
+            <div>
                 <label className="etiqueta" htmlFor="fecha_hoy">Fecha</label>
                 <input type="date" id="fecha_hoy" name="fecha_hoy" className="entrada" value={fechaActual} readOnly />
                 <label className="etiqueta" htmlFor="nombre_oftal">Nombre del oftalmólogo</label>
                 <input type="text" id="nombre_oftal" name="nombre_oftal" className="entrada" value={nombre} readOnly />
                 <label className="etiqueta" htmlFor="cedula">Cedula profesional</label>
                 <input type="text" id="cedula" name="cedula" className="entrada" value={cedula} readOnly />
+            </div>
 
+            <div>
                 <button className="ml-10 btn btn-guardar btn-lg btn-block"
                     onClick={() => generarPDF(nombre, cedula, fichasMedicas, detallesPacientes, fechaActual)}>
                     Descargar hoja diaria en PDF
@@ -126,7 +139,17 @@ export function TablaOftalmologo() {
                     onClick={() => generarExcel(nombre, cedula, fichasMedicas, detallesPacientes, fechaActual)}>
                     Descargar hoja diaria en Excel
                 </button>
+            </div>
 
+            <div className="pagination m-3 d-flex justify-content-end">
+                {Array.from({ length: Math.ceil(fichasMedicas.length / fichasPorPagina) }, (_, index) => (
+                    <button key={index + 1} onClick={() => paginate(index + 1)} className="page-link">
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
+
+            <div className="">
                 <table className="mt-3 table table-bordered border-dark table-hover">
                     <thead className="">
                         <tr className="">
@@ -141,7 +164,7 @@ export function TablaOftalmologo() {
                     </thead>
 
                     <tbody>
-                        {fichasMedicas.map((ficha, index) => (
+                        {fichasActuales.map((ficha, index) => (
                             <tr key={index}>
                                 <td className="etiqueta">{ficha.paciente}</td>
                                 <td className="etiqueta">{detallesPacientes[index]?.datosPersonalesPacient.nombre + " " + detallesPacientes[index]?.datosPersonalesPacient.apellidoP + " " + detallesPacientes[index]?.datosPersonalesPacient.apellidoM}</td>
@@ -155,6 +178,15 @@ export function TablaOftalmologo() {
                     </tbody>
                 </table>
             </div>
+
+            <div className="pagination m-3">
+                {Array.from({ length: Math.ceil(fichasMedicas.length / fichasPorPagina) }, (_, index) => (
+                    <button key={index + 1} onClick={() => paginate(index + 1)} className="page-link">
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
+            
         </div>
     );
 };
